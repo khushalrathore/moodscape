58f13cb26c1f4789f2364f3564526b2d2b5c3c70
khushalrathore <118611135+khushalrathore@users.noreply.github.com>
2023-11-25 13:09:59 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 import "../index.css"; 
 
+
 const Navbar = () => {
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
@@ -25,6 +26,20 @@ const Navbar = () => {
     <nav className={`w-full flex py-6 justify-between items-center navbar ${isFixed ? 'fixed' : ''}`}>
       <img src={logo} alt="hoobank" className="w-[175.9px] h-[37.32px]" />
 
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-orange-600" : "text-black"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img
           src={toggle ? close : menu}
@@ -35,20 +50,17 @@ const Navbar = () => {
 
         <div
           className={`${
-            !toggle ? "hidden" : "fixed inset-0 p-6 bg-black-gradient min-h-screen min-w-screen justify-end items-start flex-1 flex-col"
-          } rounded-xl sidebar`}
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex flex-col items-end">
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
                 className={`font-poppins font-medium cursor-pointer text-[16px] ${
                   active === nav.title ? "text-orange-600" : "text-black"
                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => {
-                  setActive(nav.title);
-                  setToggle(false); // Close the mobile navbar on item click
-                }}
+                onClick={() => setActive(nav.title)}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
@@ -61,3 +73,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
